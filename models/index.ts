0553a216e6449df812f5560edf7ddd6cb4ce435b
Cesar Pintos <pintos.cesar01@gmail.com>
2021-09-03 12:13:11 -0300

    Asociaciones hechas

--- models/index.ts
+++ models/index.ts
@@ -3,12 +3,36 @@ import Usuario from "./usuario";
 import Modelo from "./modelo";
 import Marca from "./marca";
 
-// Marca.hasMany(Modelo, {
-//   foreignKey: {
-//     name: 'marcaId'
-//   }
-// })
+Marca.hasMany(Modelo, {
+  foreignKey: {
+    name: "marcaId",
+    allowNull: false,
+  },
+});
+Modelo.belongsTo(Marca, {
+  foreignKey: {
+    name: "marcaId",
+    allowNull: false,
+  },
+});
+Modelo.hasMany(Auto, {
+  foreignKey: {
+    name: "modeloId",
+    allowNull: false,
+  },
+});
+Auto.belongsTo(Modelo, {
+  foreignKey: {
+    name: "modeloId",
+    allowNull: false,
+  },
+});
+Usuario.hasMany(Auto, {
+  foreignKey: {
+    name: 'propietarioId',
+    allowNull: false
+  }
+})
 
-export {
-  Auto, Usuario, Modelo, Marca
-}
+
+export { Auto, Usuario, Modelo, Marca };
