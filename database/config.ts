0dd52353d2b82d55e8ff0692d6b2df58166099f0
Cesar Pintos <pintos.cesar01@gmail.com>
2021-09-06 16:17:53 -0300

    #Agregado scripts a package.json
    ## Comandos nuevos
    
    `npm run prod` setea la bandera env=prod para que la instancia de
    Sequelize se conecte usando SSL
    
    `npm run dev` no especifica ninguna bandera, hace que valga false
    
    `npm run watchprod` y `npm run watchdev` lo mismo pero usando
    nodemon en vez de node.

--- database/config.ts
+++ database/config.ts
@@ -1,13 +1,19 @@
-import { Sequelize } from "sequelize";
+import { Options, Sequelize } from "sequelize";
+import { isProduction } from "..";
 
 const DB_NAME = process.env.DB_NAME || "db";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASS || "123456";
 const DB_HOST = process.env.DB_HOST || "localhost";
 
+const Params: Options = {
+  ssl: isProduction
+} 
+
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
+  ...Params
   // logging: false,
 });
 
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+export const isProduction = process.argv.filter(el => el.includes('env=prod'))[0] ? true : false
+
 import Server from "./server";
 
 const App = new Server();
