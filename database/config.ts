0b988a355e2b7f7bae51e69ae4231914b2932f47
Cesar Pintos <pintos.cesar01@gmail.com>
2021-09-06 14:12:54 -0300

    Cambiada configuración de ENV en database config

--- database/config.ts
+++ database/config.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const DB_NAME = process.env.DB_NAME || "db";
+const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASS || "123456";
+const DB_HOST = process.env.DB_HOST || "localhost";
 
-const db = new Sequelize(DB_NAME, "root", DB_PASSWORD, {
-  host: "localhost",
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
   // logging: false,
 });
